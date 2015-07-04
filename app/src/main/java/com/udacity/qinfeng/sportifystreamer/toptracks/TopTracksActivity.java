306d9d720212927b5f79c891e894f333c4d71140
feng qin <qinfeng0153@gmail.com>
2015-07-04 11:25:55 +0200

    changed to AppCompatActivity

--- app/src/main/java/com/udacity/qinfeng/sportifystreamer/toptracks/TopTracksActivity.java
+++ app/src/main/java/com/udacity/qinfeng/sportifystreamer/toptracks/TopTracksActivity.java
@@ -1,11 +1,11 @@
 package com.udacity.qinfeng.sportifystreamer.toptracks;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 
 import com.udacity.qinfeng.sportifystreamer.R;
 
-public class TopTracksActivity extends ActionBarActivity implements TopTracksFragment.OnFragmentInteractionListener{
+public class TopTracksActivity extends AppCompatActivity implements TopTracksFragment.OnFragmentInteractionListener{
 
     public static final String KEY_PARAM_ARTIST_NAME = "artist_name";
 
