4247d688414c0af6abbdbb4247dd5ff669103be5
feng qin <qinfeng0153@gmail.com>
2015-07-04 11:24:19 +0200

    menu deleted. Seems that we don't need menu for this project.

--- app/src/main/java/com/udacity/qinfeng/sportifystreamer/toptracks/TopTracksActivity.java
+++ app/src/main/java/com/udacity/qinfeng/sportifystreamer/toptracks/TopTracksActivity.java
@@ -2,8 +2,6 @@ package com.udacity.qinfeng.sportifystreamer.toptracks;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.udacity.qinfeng.sportifystreamer.R;
 
@@ -19,32 +17,6 @@ public class TopTracksActivity extends ActionBarActivity implements TopTracksFra
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setSubtitle(getIntent().getStringExtra(KEY_PARAM_ARTIST_NAME));
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_top_tracks, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }else if(id == android.R.id.home){
-            finish();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
 
     @Override
     public String getArtistId() {
